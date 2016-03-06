426678b824663f95f25454bf4acd6b2e135eb2e0
bsmukasa <bsmukasa@gmail.com>
2016-03-05 19:05:25 -0500

    Add set discard challenge.

---
+++ hacker_rank/python/sets/set_discard.py
@@ -0,0 +1,22 @@
+"""Set .discard()
+
+You have a non-empty set ss, and you have to execute N commands given in N lines.
+The commands will be pop, remove and discard.
+
+The first line contains integer n, the number of elements in the set s.
+The second line contains n space separated elements of set s. All of the elements are non-negative integers,
+less than or equal to 9.
+The third line contains integer N, the number of commands.
+The next N lines contains either pop, remove and/or discard commands followed by their associated value.
+
+"""
+element_count = int(input().strip())
+set_s = set(map(int, input().strip().split()))
+for idx in range(int(input().strip())):
+    row = input().strip().split()
+    if row[0] == 'pop':
+        getattr(set_s, row[0])()
+    else:
+        getattr(set_s, row[0])(int(row[1]))
+
+print(sum(set_s))
