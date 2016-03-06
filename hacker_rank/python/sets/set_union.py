872a0fdf8145c41439a9636b93cc5bbf03a2ce29
bsmukasa <bsmukasa@gmail.com>
2016-03-05 19:16:49 -0500

    Add set union challenge.

---
+++ hacker_rank/python/sets/set_union.py
@@ -0,0 +1,20 @@
+"""Set Union
+
+You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is
+subscribed to the French newspaper. The same student could be in both sets. Your task is to find the total number of
+students who have subscribed to at least one newspaper.
+
+The first line contains an integer, nn, the number of students who have subscribed to the English newspaper.
+The second line contains nn space separated roll numbers of those students.
+The third line contains bb, the number of students who have subscribed to the French newspaper.
+The fourth line contains bb space separated roll numbers of those students.
+
+"""
+english, french = (
+    set(map(int, input().strip().split()))
+    if int(input().strip()) > 0
+    else set()
+    for _ in range(2)
+)
+
+print(len(english | french))
